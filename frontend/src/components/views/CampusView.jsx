454ad57bb81f7a99b66c4f3436ddba4c038f0eff
Khinshan Khan <khanshan501@gmail.com>
2021-05-23 04:10:17 -0400

    display list of students in single campus view

--- frontend/src/components/views/CampusView.jsx
+++ frontend/src/components/views/CampusView.jsx
@@ -1,23 +1,53 @@
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 import NavBarView from "./NavBarView";
+import StudentCard from "./StudentCard";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "2vw",
+  },
+});
 
 const CampusView = ({ campus }) => {
+  const classes = useStyles();
+
   return (
     <>
       <NavBarView />
-      <div>
-        {campus && campus.students ? (
+      <div className={classes.root}>
+        {campus ? (
           <>
-            <h1>{campus.name}</h1>
+            <h1>
+              {campus.name}
+              <Link to={`/editcampus/${campus.id}`}>
+                <Button
+                  style={{ float: "right" }}
+                  variant="contained"
+                  color="primary"
+                >
+                  Edit Campus
+                </Button>
+              </Link>
+            </h1>
             <p>{campus.description}</p>
-            <ul>
-              {campus.students.map((student) => {
-                let name = student.firstname + " " + student.lastname;
-                return <li key={student.id}>{name}</li>;
-              })}
-            </ul>
-            <Link to={`/editcampus/${campus.id}`}>Edit</Link>
+
+            {campus.students.length ? (
+              <div>
+                <Grid container spacing={1}>
+                  {campus.students.map((student, index) => (
+                    <Grid key={student.id} item md={3}>
+                      <StudentCard student={student} />
+                    </Grid>
+                  ))}
+                </Grid>
+              </div>
+            ) : (
+              <div>This campus has no registered students.</div>
+            )}
           </>
         ) : (
           <div> Loading... </div>
